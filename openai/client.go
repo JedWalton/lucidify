5cd38b44e6608b82ee1da3ddea6fc09c59468621
Jed Walton <jedwaltondev@gmail.com>
2023-09-04 17:16:00 +0100

    clean

--- openai/client.go
+++ openai/client.go
@@ -10,50 +10,10 @@ type Client struct {
 	responseParser     *responseParser
 }
 
-type CompletionResponse struct {
-	ID      string   `json:"id"`
-	Object  string   `json:"object"`
-	Created int64    `json:"created"`
-	Model   string   `json:"model"`
-	Choices []Choice `json:"choices"`
-	Usage   Usage    `json:"usage"`
-}
-
-type Choice struct {
-	Index        int     `json:"index"`
-	Message      Message `json:"message"`
-	FinishReason string  `json:"finish_reason"`
-}
-
-type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
-}
-
 func NewClient(apiKey string) *Client {
 	return &Client{
 		requestConstructor: &requestConstructor{APIKey: apiKey},
 		executor:           &executor{client: &http.Client{}},
 		responseParser:     &responseParser{},
 	}
-}
-
-func (c *Client) ChatCompletion(prompt string) (*CompletionResponse, error) {
-	req, err := c.requestConstructor.construct(prompt)
-	if err != nil {
-		return nil, err
-	}
-
-	respBody, err := c.executor.execute(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.responseParser.parse(respBody)
 }
