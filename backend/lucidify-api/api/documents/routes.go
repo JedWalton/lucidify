fa06129e9d42ee64c121c09bc52f990609dac896
Jed Walton <jedwaltondev@gmail.com>
2023-09-20 15:22:28 +0100

    +

--- backend/lucidify-api/api/documents/routes.go
+++ backend/lucidify-api/api/documents/routes.go
@@ -3,13 +3,12 @@ package documents
 import (
 	"lucidify-api/modules/config"
 	"lucidify-api/modules/middleware"
-	"lucidify-api/modules/store"
 	"net/http"
 )
 
-func SetupRoutes(config *config.ServerConfig, mux *http.ServeMux, store *store.Store) *http.ServeMux {
+func SetupRoutes(config *config.ServerConfig, mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("/documents/upload", middleware.Chain(
-		DocumentsUploadHandler(store),
+		DocumentsUploadHandler(config.Store),
 		middleware.CORSMiddleware(config.AllowedOrigins),
 		middleware.Logging,
 	))
