85e5ea4f549ec4edb9e942e4eabbee648661eb42
Jed Walton <jedwaltondev@gmail.com>
2023-09-29 16:23:10 +0100

    declared interface

--- backend/lucidify-api/modules/store/store/store.go
+++ backend/lucidify-api/modules/store/store/store.go
@@ -1,10 +1,23 @@
 package store
 
-//
-// type DocumentService interface {
-// 	UploadDocument(userID, name, content string) error
-// 	GetDocument(userID, name string) (*Document, error)
-// 	GetAllDocuments(userID string) ([]Document, error)
-// 	UpdateDocumentContent(userID, name, content string) error
-// 	DeleteDocument(userID, name string) error
-// }
+import (
+	"lucidify-api/modules/store/postgresqlclient"
+	"lucidify-api/modules/store/weaviateclient"
+)
+
+type DocumentService interface {
+	// UploadDocument(userID, name, content string) error
+	// GetDocument(userID, name string) (postgresqlclient.Document, error)
+	// GetAllDocuments(userID string) ([]postgresqlclient.Document, error)
+	// UpdateDocumentContent(userID, name, content string) error
+	// DeleteDocument(userID, name string) error
+}
+
+type DocumentServiceImpl struct {
+	postgresqlDB postgresqlclient.PostgreSQL
+	weaviateDB   weaviateclient.WeaviateClient
+}
+
+func NewDocumentService(postgresqlDB postgresqlclient.PostgreSQL, weaviateDB weaviateclient.WeaviateClient) DocumentService {
+	return &DocumentServiceImpl{postgresqlDB: postgresqlDB, weaviateDB: weaviateDB}
+}
