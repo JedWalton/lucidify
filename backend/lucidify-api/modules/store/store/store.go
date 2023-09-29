1376af91c0fdfe7dec6c418b4be812213ef2c5f5
Jed Walton <jedwaltondev@gmail.com>
2023-09-29 17:20:44 +0100

    upload document

--- backend/lucidify-api/modules/store/store/store.go
+++ backend/lucidify-api/modules/store/store/store.go
@@ -1,16 +1,20 @@
 package store
 
 import (
+	"fmt"
 	"lucidify-api/modules/store/postgresqlclient"
 	"lucidify-api/modules/store/weaviateclient"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type DocumentService interface {
-	// UploadDocument(userID, name, content string) error
-	// GetDocument(userID, name string) (postgresqlclient.Document, error)
+	UploadDocument(userID, name, content string) (uuid.UUID, error)
+	GetDocument(userID, name string) (*postgresqlclient.Document, error)
 	// GetAllDocuments(userID string) ([]postgresqlclient.Document, error)
 	// UpdateDocumentContent(userID, name, content string) error
-	// DeleteDocument(userID, name string) error
+	DeleteDocument(userID, name, documentUUID string) error
 }
 
 type DocumentServiceImpl struct {
@@ -18,18 +22,64 @@ type DocumentServiceImpl struct {
 	weaviateDB   weaviateclient.WeaviateClient
 }
 
-func NewDocumentService(postgresqlDB postgresqlclient.PostgreSQL, weaviateDB weaviateclient.WeaviateClient) DocumentService {
-	return &DocumentServiceImpl{postgresqlDB: postgresqlDB, weaviateDB: weaviateDB}
+func NewDocumentService(postgresqlDB *postgresqlclient.PostgreSQL, weaviateDB weaviateclient.WeaviateClient) DocumentService {
+	return &DocumentServiceImpl{postgresqlDB: *postgresqlDB, weaviateDB: weaviateDB}
 }
 
-func (d *DocumentServiceImpl) UploadDocument(userID, name, content string) error {
-	document_uuid, err := d.postgresqlDB.UploadDocument(userID, name, content)
+// func (d *DocumentServiceImpl) UploadDocument(userID, name, content string) error {
+// 	document_uuid, err := d.postgresqlDB.UploadDocument(userID, name, content)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = d.weaviateDB.UploadDocument(document_uuid.String(), userID, name, content)
+// 	if err != nil {
+// 		err = d.postgresqlDB.DeleteDocument(userID, name)
+// 		return err
+// 	}
+// 	return nil
+// }
+
+func (d *DocumentServiceImpl) UploadDocument(userID, name, content string) (uuid.UUID, error) {
+	documentUUID, err := d.postgresqlDB.UploadDocument(userID, name, content)
+	if err != nil {
+		return documentUUID, fmt.Errorf("failed to upload document to PostgreSQL: %w", err)
+	}
+
+	const maxRetries = 3
+	const retryDelay = time.Second * 2
+	err = d.weaviateDB.UploadDocument(documentUUID.String(), userID, name, content)
+	if err != nil {
+		// Attempt to rollback the PostgreSQL upload.
+		var delErr error
+		for i := 0; i < maxRetries; i++ {
+			delErr = d.postgresqlDB.DeleteDocument(userID, name)
+			if delErr == nil {
+				break
+			}
+			// Sleep before retrying
+			time.Sleep(retryDelay)
+		}
+
+		if delErr != nil {
+			return documentUUID, fmt.Errorf("failed to upload document to Weaviate: %w; failed to delete document from PostgreSQL after %d retries: %v", err, maxRetries, delErr)
+		}
+		return documentUUID, fmt.Errorf("failed to upload document to Weaviate: %w; document deleted from PostgreSQL", err)
+	}
+
+	return documentUUID, nil
+}
+
+func (d *DocumentServiceImpl) GetDocument(userID, name string) (*postgresqlclient.Document, error) {
+	return d.postgresqlDB.GetDocument(userID, name)
+}
+
+func (d *DocumentServiceImpl) DeleteDocument(userID, name, documentUUID string) error {
+	err := d.postgresqlDB.DeleteDocument(userID, name)
 	if err != nil {
 		return err
 	}
-	err = d.weaviateDB.UploadDocument(document_uuid.String(), userID, name, content)
+	err = d.weaviateDB.DeleteDocument(documentUUID)
 	if err != nil {
-		err = d.postgresqlDB.DeleteDocument(userID, name)
 		return err
 	}
 	return nil
---
+++ backend/lucidify-api/modules/store/store/store_integration_test.go
@@ -0,0 +1,98 @@
+// //go:build integration
+// // +build integration
+package store
+
+import (
+	"log"
+	"lucidify-api/modules/config"
+	"lucidify-api/modules/store/postgresqlclient"
+	"lucidify-api/modules/store/weaviateclient"
+	"testing"
+)
+
+func createTestUserInDb() string {
+	testconfig := config.NewServerConfig()
+	PostgresqlURL := testconfig.PostgresqlURL
+	db, err := postgresqlclient.NewPostgreSQL(PostgresqlURL)
+
+	// the user id registered by the jwt token must exist in the local database
+	user := postgresqlclient.User{
+		UserID:           "TestStoreIntegrationTestUUID",
+		ExternalID:       "TestStoreIntegrationTest",
+		Username:         "TestStoreIntegrationTest",
+		PasswordEnabled:  true,
+		Email:            "TestStoreIntegrationTest@example.com",
+		FirstName:        "TestStoreIntegrationTestFirstName",
+		LastName:         "TestStoreIntegrationTestLastName",
+		ImageURL:         "https://TestStoreIntegrationTestURL.com/image.jpg",
+		ProfileImageURL:  "https://TestStoreIntegrationTestProfileURL.com/profile.jpg",
+		TwoFactorEnabled: false,
+		CreatedAt:        1654012591514,
+		UpdatedAt:        1654012591514,
+	}
+
+	db.DeleteUserInUsersTable(user.UserID)
+	err = db.CheckUserDeletedInUsersTable(user.UserID, 3)
+	if err != nil {
+		log.Fatalf("Failed to delete user: %v", err)
+	}
+	err = db.CreateUserInUsersTable(user)
+	if err != nil {
+		log.Fatalf("Failed to create user: %v", err)
+	}
+
+	// Check if the user exists
+	err = db.CheckIfUserInUsersTable(user.UserID, 3)
+	if err != nil {
+		log.Fatalf("User not found after creation: %v", err)
+	}
+	return user.UserID
+}
+
+func TestUploadDocumentIntegration(t *testing.T) {
+	// Initialize the PostgreSQL client
+	config := config.NewServerConfig()
+	postgresqlURL := config.PostgresqlURL
+	postgresqlDB, err := postgresqlclient.NewPostgreSQL(postgresqlURL)
+	if err != nil {
+		t.Fatalf("failed to initialize PostgreSQL client: %v", err)
+	}
+
+	// Initialize the Weaviate client
+	weaviateDB, err := weaviateclient.NewWeaviateClient()
+	if err != nil {
+		t.Fatalf("failed to initialize Weaviate client: %v", err)
+	}
+
+	// Initialize the DocumentService
+	documentService := NewDocumentService(postgresqlDB, weaviateDB)
+
+	// Define test document parameters
+	userID := createTestUserInDb()
+	name := "test-document-name"
+	content := "test-document-content"
+
+	// Attempt to upload the document
+	documentUUID, err := documentService.UploadDocument(userID, name, content)
+	if err != nil {
+		t.Fatalf("failed to upload document: %v", err)
+	}
+
+	// Verify that the document was uploaded to PostgreSQL
+	doc, err := postgresqlDB.GetDocument(userID, name)
+	if err != nil || doc == nil {
+		t.Fatalf("failed to retrieve document from PostgreSQL: %v", err)
+	}
+
+	// Verify that the document was uploaded to Weaviate
+	doc2, err := weaviateDB.GetDocument(documentUUID.String())
+	if err != nil || doc2 == nil {
+		t.Fatalf("failed to retrieve document from Weaviate: %v", err)
+	}
+
+	// Clean up: delete the uploaded document
+	err = documentService.DeleteDocument(userID, name, documentUUID.String())
+	if err != nil {
+		t.Fatalf("failed to delete test document: %v", err)
+	}
+}
