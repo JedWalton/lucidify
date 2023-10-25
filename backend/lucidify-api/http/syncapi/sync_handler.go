a4db92aa1de5e317cfa6083c3535dd53fb846b93
Jed Walton <jedwaltondev@gmail.com>
2023-10-25 02:15:21 +0100

    +

--- backend/lucidify-api/http/syncapi/sync_handler.go
+++ backend/lucidify-api/http/syncapi/sync_handler.go
@@ -14,6 +14,42 @@ type ServerResponse struct {
 	Message string      `json:"message,omitempty"` // Descriptive message, especially useful in case of errors
 }
 
+// LocalStorageKey defines valid keys for LocalStorage operations.
+type LocalStorageKey string
+
+const (
+	ApiKey               LocalStorageKey = "apiKey"
+	ConversationHistory  LocalStorageKey = "conversationHistory"
+	SelectedConversation LocalStorageKey = "selectedConversation"
+	Theme                LocalStorageKey = "theme"
+	Folders              LocalStorageKey = "folders"
+	Prompts              LocalStorageKey = "prompts"
+	ShowChatbar          LocalStorageKey = "showChatbar"
+	ShowPromptbar        LocalStorageKey = "showPromptbar"
+	PluginKeys           LocalStorageKey = "pluginKeys"
+	Settings             LocalStorageKey = "settings"
+)
+
+// IsValid checks if the provided key is a valid LocalStorageKey.
+func (key LocalStorageKey) IsValid() bool {
+	switch key {
+	case ApiKey, ConversationHistory, SelectedConversation, Theme, Folders,
+		Prompts, ShowChatbar, ShowPromptbar, PluginKeys, Settings:
+		return true
+	}
+	return false
+}
+
+// This is a utility function to send JSON responses
+func sendJSONResponse(w http.ResponseWriter, statusCode int, response ServerResponse) {
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func SyncHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json") // Set content type for all responses from this handler
@@ -23,7 +59,13 @@ func SyncHandler() http.HandlerFunc {
 		switch r.Method {
 		case http.MethodGet, http.MethodDelete, http.MethodPost:
 			// For GET, DELETE, and POST, read 'key' from query parameters
-			key := r.URL.Query().Get("key")
+			// key := r.URL.Query().Get("key")
+			key := LocalStorageKey(r.URL.Query().Get("key"))
+			// Validate the key
+			if !key.IsValid() {
+				// Handle invalid key: return an error response, etc.
+				return
+			}
 			if key == "" {
 				response := ServerResponse{
 					Success: false,
@@ -40,9 +82,9 @@ func SyncHandler() http.HandlerFunc {
 			}
 
 			if r.Method == http.MethodGet {
-				syncservice.FetchData(w, r, key)
+				syncservice.FetchData(w, r, string(key))
 			} else if r.Method == http.MethodDelete {
-				syncservice.DeleteData(w, r, key)
+				syncservice.DeleteData(w, r, string(key))
 			} else {
 				// For POST, read 'value' from the request body
 				var requestData map[string]interface{}
@@ -60,7 +102,7 @@ func SyncHandler() http.HandlerFunc {
 
 				// Now you have the 'key' from the URL and 'value' from the request body and can proceed
 				// You might want to modify your 'syncDataToDB' function to accept both 'key' and 'value'
-				err = syncservice.SyncData(key, value) // Make sure this function accepts both key and value
+				err = syncservice.SyncData(string(key), value) // Make sure this function accepts both key and value
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
