f7df18997bb10b9b24c7c3dfb56ee63e08f67c35
Jed Walton <jedwaltondev@gmail.com>
2023-10-25 23:56:18 +0100

    +

--- backend/lucidify-api/http/syncapi/sync_handler.go
+++ backend/lucidify-api/http/syncapi/sync_handler.go
@@ -6,41 +6,42 @@ import (
 	"net/http"
 )
 
-// ServerResponse is the structure that defines the standard response from the server.
-type ServerResponse struct {
-	Success bool        `json:"success"`           // Indicates if the operation was successful
-	Data    interface{} `json:"data,omitempty"`    // Holds the actual data, if any
-	Message string      `json:"message,omitempty"` // Descriptive message, especially useful in case of errors
-}
-
 // LocalStorageKey defines valid keys for LocalStorage operations.
 type LocalStorageKey string
 
 const (
 	apiKey               LocalStorageKey = "apiKey"
-	ConversationHistory  LocalStorageKey = "conversationHistory"
-	SelectedConversation LocalStorageKey = "selectedConversation"
-	Theme                LocalStorageKey = "theme"
-	Folders              LocalStorageKey = "folders"
-	Prompts              LocalStorageKey = "prompts"
-	ShowChatbar          LocalStorageKey = "showChatbar"
-	ShowPromptbar        LocalStorageKey = "showPromptbar"
-	PluginKeys           LocalStorageKey = "pluginKeys"
-	Settings             LocalStorageKey = "settings"
+	conversationHistory  LocalStorageKey = "conversationHistory"
+	selectedConversation LocalStorageKey = "selectedConversation"
+	theme                LocalStorageKey = "theme"
+	folders              LocalStorageKey = "folders"
+	prompts              LocalStorageKey = "prompts"
+	showChatbar          LocalStorageKey = "showChatbar"
+	showPromptbar        LocalStorageKey = "showPromptbar"
+	pluginKeys           LocalStorageKey = "pluginKeys"
+	settings             LocalStorageKey = "settings"
 )
 
 // IsValid checks if the provided key is a valid LocalStorageKey.
 func (key LocalStorageKey) IsValid() bool {
 	switch key {
-	case apiKey, ConversationHistory, SelectedConversation, Theme, Folders,
-		Prompts, ShowChatbar, ShowPromptbar, PluginKeys, Settings:
+	case apiKey, conversationHistory, selectedConversation, theme, folders,
+		prompts, showChatbar, showPromptbar, pluginKeys, settings:
 		return true
 	}
 	return false
 }
 
+func MethodNotAllowed(w http.ResponseWriter) {
+	response := syncservice.ServerResponse{
+		Success: false,
+		Message: "Method not allowed",
+	}
+	sendJSONResponse(w, http.StatusMethodNotAllowed, response)
+}
+
 // This is a utility function to send JSON responses
-func sendJSONResponse(w http.ResponseWriter, statusCode int, response ServerResponse) {
+func sendJSONResponse(w http.ResponseWriter, statusCode int, response syncservice.ServerResponse) {
 	w.WriteHeader(statusCode)
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
@@ -49,21 +50,50 @@ func sendJSONResponse(w http.ResponseWriter, statusCode int, response ServerResp
 	}
 }
 
+//	func SyncHandler() http.HandlerFunc {
+//		return func(w http.ResponseWriter, r *http.Request) {
+//			w.Header().Set("Content-Type", "application/json")
+//			key := r.URL.Query().Get("key")
+//
+//			// Let syncservice handle all logic, validation, and response generation
+//			switch r.Method {
+//			case http.MethodGet:
+//				syncservice.HandleGet(key)
+//			case http.MethodDelete:
+//				syncservice.HandleRemove(key)
+//			case http.MethodPost:
+//				syncservice.HandleSet(key, r.Body)
+//			default:
+//				syncservice.MethodNotAllowed(w)
+//			}
+//		}
+//	}
 func SyncHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		key := r.URL.Query().Get("key")
 
-		// Let syncservice handle all logic, validation, and response generation
+		var response syncservice.ServerResponse
+
 		switch r.Method {
 		case http.MethodGet:
-			syncservice.HandleGet(key)
+			data, resp := syncservice.HandleGet(key)
+			response = resp
+			if response.Success {
+				response.Data = data
+			}
 		case http.MethodDelete:
-			syncservice.HandleRemove(key)
+			response = syncservice.HandleRemove(key)
 		case http.MethodPost:
-			syncservice.HandleSet(key, r.Body)
+			response = syncservice.HandleSet(key, r.Body)
 		default:
-			syncservice.MethodNotAllowed(w)
+			response = syncservice.ServerResponse{
+				Success: false,
+				Message: "Method not allowed",
+			}
+			sendJSONResponse(w, http.StatusMethodNotAllowed, response)
+			return
 		}
+		sendJSONResponse(w, http.StatusOK, response)
 	}
 }
