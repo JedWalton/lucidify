3876d16ae4f6c702e185ffc290d856837344f180
Jed Walton <jedwaltondev@gmail.com>
2023-10-25 22:08:51 +0100

    incrementing

--- backend/lucidify-api/service/syncservice/sync_service.go
+++ backend/lucidify-api/service/syncservice/sync_service.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
 )
 
 // ServerResponse is the structure that defines the standard response from the server.
@@ -27,18 +26,18 @@ func sendJSONResponse(w http.ResponseWriter, statusCode int, response ServerResp
 func FetchData(w http.ResponseWriter, r *http.Request, key string) {
 	log.Printf("FetchData called with key: %s\n", key)
 
-	fetchedData, exists := GetDataFromServerDB(key)
-	if !exists {
-		response := ServerResponse{
-			Success: false,
-			Message: "No data found for key: " + key,
-		}
-		sendJSONResponse(w, http.StatusInternalServerError, response)
-		return
-	}
+	// fetchedData, exists := GetDataFromServerDB(key)
+	// if !exists {
+	// 	response := ServerResponse{
+	// 		Success: false,
+	// 		Message: "No data found for key: " + key,
+	// 	}
+	// 	sendJSONResponse(w, http.StatusInternalServerError, response)
+	// 	return
+	// }
 	serverResponse := ServerResponse{
 		Success: true,
-		Data:    fetchedData,
+		Data:    "hello",
 		Message: "Data fetched successfully",
 	}
 
@@ -48,6 +47,7 @@ func FetchData(w http.ResponseWriter, r *http.Request, key string) {
 func DeleteData(w http.ResponseWriter, r *http.Request, key string) {
 	response := ServerResponse{
 		Success: true,
+		Data:    "deleted placeholder",
 		Message: "Data deleted successfully",
 	}
 
@@ -61,7 +61,7 @@ func SyncData(key string, value interface{}) error {
 	// Here, you would write your logic to sync data to your database.
 	// This might include SQL statements, or calls to another service, etc.
 
-	SetDataInServerDB(key, value)
+	// SetDataInServerDB(key, value)
 	// For demonstration, we'll just print the key and value.
 	log.Printf("Syncing data to DB - Key: %s, Value: %v", key, value)
 
@@ -69,33 +69,106 @@ func SyncData(key string, value interface{}) error {
 	return nil
 }
 
-// Global LocalStorage and its mutex
-var (
-	localStorage      = make(map[string]interface{})
-	localStorageMutex sync.RWMutex
-)
+var Storage LocalStorage // Global variable representing our local storage
 
-// SetData sets a key-value pair in the local storage
-func SetDataInServerDB(key string, value interface{}) {
-	localStorageMutex.Lock()
-	defer localStorageMutex.Unlock()
-
-	localStorage[key] = value
+// GetDataFromLocalStorage retrieves a value from local storage based on key.
+func GetDataFromLocalStorage(key string) (interface{}, bool) {
+	switch key {
+	case "APIKey":
+		return Storage.APIKey, true
+	case "ConversationHistory":
+		return Storage.ConversationHistory, true
+	case "SelectedConversation":
+		return Storage.SelectedConversation, true
+	case "Theme":
+		return Storage.Theme, true
+	case "Folders":
+		return Storage.Folders, true
+	case "Prompts":
+		return Storage.Prompts, true
+	case "ShowChatbar":
+		return Storage.ShowChatbar, true
+	case "ShowPromptbar":
+		return Storage.ShowPromptbar, true
+	case "PluginKeys":
+		return Storage.PluginKeys, true
+	case "Settings":
+		return Storage.Settings, true
+	case "CHANGELOG":
+		return Storage.CHANGELOG, true
+	default:
+		return nil, false
+	}
 }
 
-// GetData retrieves the value for a given key from the local storage
-func GetDataFromServerDB(key string) (interface{}, bool) {
-	localStorageMutex.RLock()
-	defer localStorageMutex.RUnlock()
-
-	value, exists := localStorage[key]
-	return value, exists
+// SetDataInLocalStorage sets a value in local storage based on key.
+func SetDataInLocalStorage(key string, value interface{}) bool {
+	switch key {
+	case "APIKey":
+		Storage.APIKey = value.(string)
+	case "ConversationHistory":
+		Storage.ConversationHistory = value.([]Conversation)
+	case "SelectedConversation":
+		Storage.SelectedConversation = value.(Conversation)
+	case "Theme":
+		Storage.Theme = value.(string)
+	case "Folders":
+		Storage.Folders = value.([]FolderInterface)
+	case "Prompts":
+		Storage.Prompts = value.([]Prompt)
+	case "ShowChatbar":
+		Storage.ShowChatbar = value.(bool)
+	case "ShowPromptbar":
+		Storage.ShowPromptbar = value.(bool)
+	case "PluginKeys":
+		Storage.PluginKeys = value.([]PluginKey)
+	case "Settings":
+		Storage.Settings = value.(Settings)
+	case "CHANGELOG":
+		Storage.CHANGELOG = value.(*[]ChangeLog)
+	default:
+		return false
+	}
+	return true
 }
 
-// DeleteData removes a key-value pair from the local storage
-func DeleteDataFromServerDB(key string) {
-	localStorageMutex.Lock()
-	defer localStorageMutex.Unlock()
-
-	delete(localStorage, key)
+// RemoveDataFromLocalStorage removes a value from local storage based on key.
+func RemoveDataFromLocalStorage(key string) bool {
+	switch key {
+	case "APIKey":
+		Storage.APIKey = ""
+		return true
+	case "ConversationHistory":
+		Storage.ConversationHistory = nil
+		return true
+	case "SelectedConversation":
+		Storage.SelectedConversation = Conversation{}
+		return true
+	case "Theme":
+		Storage.Theme = ""
+		return true
+	case "Folders":
+		Storage.Folders = nil
+		return true
+	case "Prompts":
+		Storage.Prompts = nil
+		return true
+	case "ShowChatbar":
+		Storage.ShowChatbar = false
+		return true
+	case "ShowPromptbar":
+		Storage.ShowPromptbar = false
+		return true
+	case "PluginKeys":
+		Storage.PluginKeys = nil
+		return true
+	case "Settings":
+		Storage.Settings = Settings{}
+		return true
+	case "CHANGELOG":
+		Storage.CHANGELOG = nil
+		return true
+	default:
+		return false
+	}
 }
