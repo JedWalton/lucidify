73365f312cd359fc03aa157b337a6f5ec53d8cb5
Jed Walton <jedwaltondev@gmail.com>
2023-10-26 23:09:44 +0100

    +

--- backend/lucidify-api/http/syncapi/sync_handler.go
+++ backend/lucidify-api/http/syncapi/sync_handler.go
@@ -8,6 +8,40 @@ import (
 	"net/http"
 )
 
+// LocalStorageKey defines valid keys for LocalStorage operations.
+type LocalStorageKey string
+
+const (
+	apiKey               LocalStorageKey = "apiKey"
+	conversationHistory  LocalStorageKey = "conversationHistory"
+	selectedConversation LocalStorageKey = "selectedConversation"
+	theme                LocalStorageKey = "theme"
+	folders              LocalStorageKey = "folders"
+	prompts              LocalStorageKey = "prompts"
+	showChatbar          LocalStorageKey = "showChatbar"
+	showPromptbar        LocalStorageKey = "showPromptbar"
+	pluginKeys           LocalStorageKey = "pluginKeys"
+	settings             LocalStorageKey = "settings"
+)
+
+// IsValid checks if the provided key is a valid LocalStorageKey.
+func (key LocalStorageKey) IsValid() bool {
+	switch key {
+	case apiKey, conversationHistory, selectedConversation, theme, folders,
+		prompts, showChatbar, showPromptbar, pluginKeys, settings:
+		return true
+	}
+	return false
+}
+
+func MethodNotAllowed(w http.ResponseWriter) {
+	response := syncservice.ServerResponse{
+		Success: false,
+		Message: "Method not allowed",
+	}
+	sendJSONResponse(w, http.StatusMethodNotAllowed, response)
+}
+
 // This is a utility function to send JSON responses
 func sendJSONResponse(w http.ResponseWriter, statusCode int, response syncservice.ServerResponse) {
 	w.WriteHeader(statusCode)
@@ -22,6 +56,14 @@ func SyncHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		key := r.URL.Query().Get("key")
+		if !LocalStorageKey(key).IsValid() {
+			response := syncservice.ServerResponse{
+				Success: false,
+				Message: "Invalid key",
+			}
+			sendJSONResponse(w, http.StatusBadRequest, response)
+			return
+		}
 
 		bodyBytes, err := io.ReadAll(r.Body)
 		if err != nil {
@@ -35,11 +77,7 @@ func SyncHandler() http.HandlerFunc {
 
 		switch r.Method {
 		case http.MethodGet:
-			resp := syncservice.HandleGet(key)
-			response = resp
-			if response.Success {
-				response.Data = resp.Data
-			}
+			response = syncservice.HandleGet(key)
 		case http.MethodDelete:
 			response = syncservice.HandleRemove(key)
 		case http.MethodPost:
