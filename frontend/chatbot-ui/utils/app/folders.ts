6ba067e12c3d007896916ad8053c245adda4a852
Jed Walton <jedwaltondev@gmail.com>
2023-10-30 00:33:41 +0000

    folders persisted too

--- frontend/chatbot-ui/utils/app/folders.ts
+++ frontend/chatbot-ui/utils/app/folders.ts
@@ -1,5 +1,7 @@
+import { storageService } from '@/services/storageService';
 import { FolderInterface } from '@/types/folder';
 
-export const saveFolders = (folders: FolderInterface[]) => {
+export const saveFolders = async (folders: FolderInterface[]) => {
   localStorage.setItem('folders', JSON.stringify(folders));
+  await storageService.setItemOnServer('folders', JSON.stringify(folders));
 };
