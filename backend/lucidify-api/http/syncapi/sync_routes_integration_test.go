544aedce52bb4bfaf4ce246a8b51a763d0d58ccb
Jed Walton <jedwaltondev@gmail.com>
2023-11-01 16:52:46 +0000

    working in tests

--- backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
+++ backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
@@ -10,23 +10,42 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/clerkinc/clerk-sdk-go/clerk"
 )
 
-func setupServer(t *testing.T) *httptest.Server {
+type TestSetup struct {
+	Config        *config.ServerConfig
+	ClerkInstance clerk.Client
+	SyncService   syncservice.SyncService
+	Server        *httptest.Server
+}
+
+func setupTestServer(t *testing.T) *TestSetup {
 	cfg := &config.ServerConfig{}
+
 	clerkInstance, err := clerkservice.NewClerkClient()
 	if err != nil {
 		t.Fatalf("Failed to create Clerk client: %v", err)
 	}
-	mux := http.NewServeMux()
+
 	syncService, err := syncservice.NewSyncService()
 	if err != nil {
 		t.Fatalf("Failed to create SyncService: %v", err)
 	}
-	return httptest.NewServer(SetupRoutes(cfg, mux, clerkInstance, syncService))
+
+	mux := http.NewServeMux()
+	server := httptest.NewServer(SetupRoutes(cfg, mux, clerkInstance, syncService))
+
+	return &TestSetup{
+		Config:        cfg,
+		ClerkInstance: clerkInstance,
+		SyncService:   syncService,
+		Server:        server,
+	}
 }
 
-func makeGetRequest(t *testing.T, server *httptest.Server, endpoint string) (*http.Response, string) {
+func makeGetRequestNoJWT(t *testing.T, server *httptest.Server, endpoint string) (*http.Response, string) {
 	res, err := http.Get(server.URL + endpoint)
 	if err != nil {
 		t.Fatalf("Failed to make a GET request: %v", err)
@@ -37,11 +56,12 @@ func makeGetRequest(t *testing.T, server *httptest.Server, endpoint string) (*ht
 	}
 	return res, string(bodyBytes)
 }
-func TestGetRequest(t *testing.T) {
-	server := setupServer(t)
-	defer server.Close()
 
-	res, responseBody := makeGetRequest(t, server, "/api/sync/localstorage/?key=test")
+func TestGetRequestNoJWT(t *testing.T) {
+	setup := setupTestServer(t)
+	defer setup.Server.Close()
+
+	res, responseBody := makeGetRequestNoJWT(t, setup.Server, "/api/sync/localstorage/?key=test")
 
 	expectedResponse := `couldn't find cookie __session`
 	if responseBody != expectedResponse {
@@ -51,20 +71,3 @@ func TestGetRequest(t *testing.T) {
 		t.Fatalf("Expected status BAD REQUEST; got %v", res.StatusCode)
 	}
 }
-
-// func TestInvalidEndpoint(t *testing.T) {
-// 	server := setupServer(t)
-// 	defer server.Close()
-//
-// 	res, responseBody := makeGetRequest(t, server, "/api/sync/localstorage/?key=test")
-//
-// 	expectedResponse := `{"success":false,"message":"Invalid key"}`
-// 	if responseBody != expectedResponse {
-// 		t.Fatalf("Expected response body to be %v; got %v", expectedResponse, responseBody)
-// 	}
-// 	if res.StatusCode != http.StatusBadRequest {
-// 		t.Fatalf("Expected status BAD REQUEST; got %v", res.StatusCode)
-// 	}
-// }
-//
-// Further tests can be added in a similar manner
