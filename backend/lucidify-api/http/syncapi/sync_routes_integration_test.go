ce6ad4ceff6e454f113899b6c783e09adf19386c
Jed Walton <jedwaltondev@gmail.com>
2023-11-02 03:41:21 +0000

    folders integration test

--- backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
+++ backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
@@ -247,7 +247,36 @@ func TestConversationHistoryIntegration(t *testing.T) {
 			`{"success":true,"data":"\"conversationHistory but updated!\"","message":"Data fetched successfully"}`, string(respBody))
 	}
 
-	// Unauthenticated request
+	// Unauthenticated POST request
+	req, _ = http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=conversationHistory",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken+"invalid")
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	// This is counter intuitive. Not sure why clerk doesn't return a 401.
+	if string(respBody) != `couldn't find cookie __session` {
+		t.Errorf("Expected response body %s, got %s", `couldn't find cookie __session`, string(respBody))
+	}
+	// Unauthenticated GET request
 	req, _ = http.NewRequest(
 		http.MethodGet,
 		server.URL+"/api/sync/localstorage/?key=conversationHistory",
@@ -286,67 +315,6 @@ func TestConversationHistoryIntegration(t *testing.T) {
 	})
 }
 
-func TestPromptsIntegration(t *testing.T) {
-	setup := SetupTestEnvironment(t)
-	cfg := setup.Config
-	postgresqlDB := setup.PostgresqlDB
-	clerkInstance := setup.ClerkInstance
-
-	syncService, err := syncservice.NewSyncService()
-	if err != nil {
-		t.Fatalf("Failed to create SyncService: %v", err)
-	}
-
-	// Create a test server
-	mux := http.NewServeMux()
-	SetupRoutes(cfg, mux, clerkInstance, syncService)
-	server := httptest.NewServer(mux)
-	defer server.Close()
-
-	// Obtain a JWT token from Clerk
-	jwtToken := cfg.TestJWTSessionToken
-
-	// Send a POST request to the server with the JWT token
-	body, _ := json.Marshal("prompts")
-	req, _ := http.NewRequest(http.MethodPost, server.URL+"/api/sync/localstorage/?key=prompts", bytes.NewBuffer(body))
-	req.Header.Set("Authorization", "Bearer "+jwtToken)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Errorf("Failed to send request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Check the response
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
-	}
-
-	// Send a GET request to the server with the JWT token
-	body, _ = json.Marshal("prompts")
-	req, _ = http.NewRequest(http.MethodGet, server.URL+"/api/sync/localstorage/?key=prompts", bytes.NewBuffer(body))
-	req.Header.Set("Authorization", "Bearer "+jwtToken)
-	client = &http.Client{}
-	resp, err = client.Do(req)
-	if err != nil {
-		t.Errorf("Failed to send request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Check the response
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
-	}
-
-	// Cleanup the database
-	t.Cleanup(func() {
-		testconfig := config.NewServerConfig()
-		UserID := testconfig.TestUserID
-		postgresqlDB.DeleteUserInUsersTable(UserID)
-		// postgresqlDB.DeleteDocument(UserID, "Test Document")
-	})
-}
-
 func TestFoldersIntegration(t *testing.T) {
 	setup := SetupTestEnvironment(t)
 	cfg := setup.Config
@@ -368,8 +336,14 @@ func TestFoldersIntegration(t *testing.T) {
 	jwtToken := cfg.TestJWTSessionToken
 
 	// Send a POST request to the server with the JWT token
-	body, _ := json.Marshal("folders")
-	req, _ := http.NewRequest(http.MethodPost, server.URL+"/api/sync/localstorage/?key=folders", bytes.NewBuffer(body))
+	body, _ := json.Marshal("someFoldersData")
+
+	// Authenticated request
+	req, _ := http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
 	req.Header.Set("Authorization", "Bearer "+jwtToken)
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -382,9 +356,25 @@ func TestFoldersIntegration(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
 	}
+	// Read the response body
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
 
-	body, _ = json.Marshal("folders")
-	req, _ = http.NewRequest(http.MethodGet, server.URL+"/api/sync/localstorage/?key=folders", bytes.NewBuffer(body))
+	if string(respBody) !=
+		`{"success":true,"message":"Data set successfully for key: folders"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"message":"Data set successfully for key: folders"}`,
+			string(respBody))
+	}
+
+	// Autheticated Request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
 	req.Header.Set("Authorization", "Bearer "+jwtToken)
 	client = &http.Client{}
 	resp, err = client.Do(req)
@@ -393,8 +383,144 @@ func TestFoldersIntegration(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
+	// Check the response
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"data":"\"someFoldersData\"","message":"Data fetched successfully"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"data":"\"someFoldersData\"","message":"Data fetched successfully"}`, string(respBody))
+	}
+
+	// Testing the on conflict (postgres) functionality to update chat
+	// Send a POST request to the server with the JWT token
+	body, _ = json.Marshal("someFoldersData but updated!")
+
+	// Authenticated request
+	req, _ = http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"message":"Data set successfully for key: folders"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"message":"Data set successfully for key: folders"}`,
+			string(respBody))
+	}
+
+	// Autheticated Request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"data":"\"someFoldersData but updated!\"","message":"Data fetched successfully"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"data":"\"someFoldersData but updated!\"","message":"Data fetched successfully"}`, string(respBody))
+	}
+
+	// Unauthenticated POST request
+	req, _ = http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken+"invalid")
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	// This is counter intuitive. Not sure why clerk doesn't return a 401.
+	if string(respBody) != `couldn't find cookie __session` {
+		t.Errorf("Expected response body %s, got %s", `couldn't find cookie __session`, string(respBody))
+	}
+	// Unauthenticated GET request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=folders",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken+"invalid")
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	// This is counter intuitive. Not sure why clerk doesn't return a 401.
+	if string(respBody) != `couldn't find cookie __session` {
+		t.Errorf("Expected response body %s, got %s", `couldn't find cookie __session`, string(respBody))
 	}
 
 	// Cleanup the database
@@ -405,47 +531,3 @@ func TestFoldersIntegration(t *testing.T) {
 		// postgresqlDB.DeleteDocument(UserID, "Test Document")
 	})
 }
-
-// func TestClearConversationsFromServerIntegration(t *testing.T) {
-// 	setup := setupTestServer(t)
-// 	cfg := setup.Config
-// 	syncService := setup.SyncService
-// 	clerkInstance := setup.ClerkInstance
-//
-// 	// Create a test server
-// 	mux := http.NewServeMux()
-// 	SetupRoutes(cfg, mux, clerkInstance, syncService)
-// 	server := httptest.NewServer(mux)
-// 	defer server.Close()
-//
-// 	// Obtain a JWT token from Clerk
-// 	jwtToken := cfg.TestJWTSessionToken
-//
-// 	// Send a DELETE request to the server with the JWT token
-// 	req, err := http.NewRequest(http.MethodDelete, server.URL+"/api/sync/localstorage/?key=clearConversations", bytes.NewBuffer(nil))
-// 	if err != nil {
-// 		t.Errorf("Failed to create request: %v", err)
-// 	}
-// 	req.Header.Set("Authorization", "Bearer "+jwtToken)
-//
-// 	// Add the "__session" cookie to the request
-// 	// sessionCookieValue := jwtToken
-// 	// req.AddCookie(&http.Cookie{Name: "__session", Value: sessionCookieValue})
-//
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		t.Errorf("Failed to send request: %v", err)
-// 	}
-// 	defer resp.Body.Close()
-//
-// 	// Check the response
-// 	if resp.StatusCode != http.StatusOK {
-// 		bodyBytes, _ := io.ReadAll(resp.Body)
-// 		t.Errorf("Expected status code %d, got %d. Response body: %s", http.StatusOK, resp.StatusCode, bodyBytes)
-// 	}
-//
-// 	// Check if the conversations are actually cleared
-// 	// TODO: Implement a function to verify that the conversations for the user have been cleared.
-// 	// For example, you can use a DB function like `GetConversations(userID)` and ensure the list is empty.
-// }
