ca8e81ba6c1e3ea9874c770b54e665998b456bf8
Jed Walton <jedwaltondev@gmail.com>
2023-11-02 02:19:27 +0000

    integration w

--- backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
+++ backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
@@ -3,10 +3,17 @@
 package syncapi
 
 import (
-	"io"
+	"bytes"
+	"encoding/json"
+	"log"
+	"lucidify-api/data/store/postgresqlclient"
+	"lucidify-api/data/store/storemodels"
+	"lucidify-api/data/store/weaviateclient"
 	"lucidify-api/server/config"
 	"lucidify-api/service/clerkservice"
+	"lucidify-api/service/documentservice"
 	"lucidify-api/service/syncservice"
+	"lucidify-api/service/userservice"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,60 +21,180 @@ import (
 	"github.com/clerkinc/clerk-sdk-go/clerk"
 )
 
-type TestSetup struct {
-	Config        *config.ServerConfig
-	ClerkInstance clerk.Client
-	SyncService   syncservice.SyncService
-	Server        *httptest.Server
+func createTestUserInDb() error {
+	testconfig := config.NewServerConfig()
+	db, err := postgresqlclient.NewPostgreSQL()
+
+	// the user id registered by the jwt token must exist in the local database
+	user := storemodels.User{
+		UserID:           testconfig.TestUserID,
+		ExternalID:       "TestCreateUserInUsersTableExternalIDDocuments",
+		Username:         "TestCreateUserInUsersTableUsernameDocuments",
+		PasswordEnabled:  true,
+		Email:            "TestCreateUserInUsersTableDocuments@example.com",
+		FirstName:        "TestCreateUserInUsersTableCreateTest",
+		LastName:         "TestCreateUserInUsersTableUser",
+		ImageURL:         "https://TestCreateUserInUsersTable.com/image.jpg",
+		ProfileImageURL:  "https://TestCreateUserInUsersTable.com/profile.jpg",
+		TwoFactorEnabled: false,
+		CreatedAt:        1654012591514,
+		UpdatedAt:        1654012591514,
+	}
+
+	userService, err := userservice.NewUserService()
+	if err != nil {
+		log.Fatalf("Failed to create UserService: %v", err)
+	}
+
+	err = userService.DeleteUser(user.UserID)
+	if err != nil {
+		log.Fatalf("Failed to delete user: %v", err)
+	}
+	if !userService.HasUserBeenDeleted(user.UserID, 3) {
+		log.Fatalf("Failed to delete user: %v", err)
+	}
+
+	err = db.CreateUserInUsersTable(user)
+	if err != nil {
+		log.Fatalf("Failed to create user: %v", err)
+	}
+
+	// Check if the user exists
+	_, err = userService.GetUserWithRetries(user.UserID, 3)
+	if err != nil {
+		log.Fatalf("User not found after creation: %v", err)
+	}
+
+	return nil
 }
 
-func setupTestServer(t *testing.T) *TestSetup {
-	cfg := &config.ServerConfig{}
+type TestSetup struct {
+	Config          *config.ServerConfig
+	PostgresqlDB    *postgresqlclient.PostgreSQL
+	ClerkInstance   clerk.Client
+	WeaviateDB      weaviateclient.WeaviateClient
+	DocumentService documentservice.DocumentService
+}
+
+func SetupTestEnvironment(t *testing.T) *TestSetup {
+	cfg := config.NewServerConfig()
+
+	postgresqlDB, err := postgresqlclient.NewPostgreSQL()
+	if err != nil {
+		t.Fatalf("Failed to create test postgresqlclient: %v", err)
+	}
 
 	clerkInstance, err := clerkservice.NewClerkClient()
 	if err != nil {
 		t.Fatalf("Failed to create Clerk client: %v", err)
 	}
 
+	weaviateDB, err := weaviateclient.NewWeaviateClientTest()
+	if err != nil {
+		t.Fatalf("Failed to create Weaviate client: %v", err)
+	}
+
+	err = createTestUserInDb()
+	if err != nil {
+		t.Fatalf("Failed to create test user in db: %v", err)
+	}
+
+	documentService := documentservice.NewDocumentService(postgresqlDB, weaviateDB)
+
+	return &TestSetup{
+		Config:          cfg,
+		PostgresqlDB:    postgresqlDB,
+		ClerkInstance:   clerkInstance,
+		WeaviateDB:      weaviateDB,
+		DocumentService: documentService,
+	}
+}
+
+func TestPostConversationHistoryIntegration(t *testing.T) {
+	setup := SetupTestEnvironment(t)
+	cfg := setup.Config
+	postgresqlDB := setup.PostgresqlDB
+	clerkInstance := setup.ClerkInstance
+
 	syncService, err := syncservice.NewSyncService()
 	if err != nil {
 		t.Fatalf("Failed to create SyncService: %v", err)
 	}
 
+	// Create a test server
 	mux := http.NewServeMux()
-	server := httptest.NewServer(SetupRoutes(cfg, mux, clerkInstance, syncService))
+	SetupRoutes(cfg, mux, clerkInstance, syncService)
+	server := httptest.NewServer(mux)
+	defer server.Close()
 
-	return &TestSetup{
-		Config:        cfg,
-		ClerkInstance: clerkInstance,
-		SyncService:   syncService,
-		Server:        server,
-	}
-}
+	// Obtain a JWT token from Clerk
+	jwtToken := cfg.TestJWTSessionToken
 
-func makeGetRequestNoJWT(t *testing.T, server *httptest.Server, endpoint string) (*http.Response, string) {
-	res, err := http.Get(server.URL + endpoint)
+	// Send a POST request to the server with the JWT token
+	body, _ := json.Marshal("conversationHistory")
+	req, _ := http.NewRequest(http.MethodPost, server.URL+"/api/sync/localstorage/?key=conversationHistory", bytes.NewBuffer(body))
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		t.Fatalf("Failed to make a GET request: %v", err)
+		t.Errorf("Failed to send request: %v", err)
 	}
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("Failed to read response body: %v", err)
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
 	}
-	return res, string(bodyBytes)
+
+	// Cleanup the database
+	t.Cleanup(func() {
+		testconfig := config.NewServerConfig()
+		UserID := testconfig.TestUserID
+		postgresqlDB.DeleteUserInUsersTable(UserID)
+		// postgresqlDB.DeleteDocument(UserID, "Test Document")
+	})
 }
 
-func TestGetRequestNoJWT(t *testing.T) {
-	setup := setupTestServer(t)
-	defer setup.Server.Close()
-
-	res, responseBody := makeGetRequestNoJWT(t, setup.Server, "/api/sync/localstorage/?key=test")
-
-	expectedResponse := `couldn't find cookie __session`
-	if responseBody != expectedResponse {
-		t.Fatalf("Expected response body to be %v; got %v", expectedResponse, responseBody)
-	}
-	if res.StatusCode != http.StatusBadRequest {
-		t.Fatalf("Expected status BAD REQUEST; got %v", res.StatusCode)
-	}
-}
+// func TestClearConversationsFromServerIntegration(t *testing.T) {
+// 	setup := setupTestServer(t)
+// 	cfg := setup.Config
+// 	syncService := setup.SyncService
+// 	clerkInstance := setup.ClerkInstance
+//
+// 	// Create a test server
+// 	mux := http.NewServeMux()
+// 	SetupRoutes(cfg, mux, clerkInstance, syncService)
+// 	server := httptest.NewServer(mux)
+// 	defer server.Close()
+//
+// 	// Obtain a JWT token from Clerk
+// 	jwtToken := cfg.TestJWTSessionToken
+//
+// 	// Send a DELETE request to the server with the JWT token
+// 	req, err := http.NewRequest(http.MethodDelete, server.URL+"/api/sync/localstorage/?key=clearConversations", bytes.NewBuffer(nil))
+// 	if err != nil {
+// 		t.Errorf("Failed to create request: %v", err)
+// 	}
+// 	req.Header.Set("Authorization", "Bearer "+jwtToken)
+//
+// 	// Add the "__session" cookie to the request
+// 	// sessionCookieValue := jwtToken
+// 	// req.AddCookie(&http.Cookie{Name: "__session", Value: sessionCookieValue})
+//
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		t.Errorf("Failed to send request: %v", err)
+// 	}
+// 	defer resp.Body.Close()
+//
+// 	// Check the response
+// 	if resp.StatusCode != http.StatusOK {
+// 		bodyBytes, _ := io.ReadAll(resp.Body)
+// 		t.Errorf("Expected status code %d, got %d. Response body: %s", http.StatusOK, resp.StatusCode, bodyBytes)
+// 	}
+//
+// 	// Check if the conversations are actually cleared
+// 	// TODO: Implement a function to verify that the conversations for the user have been cleared.
+// 	// For example, you can use a DB function like `GetConversations(userID)` and ensure the list is empty.
+// }
