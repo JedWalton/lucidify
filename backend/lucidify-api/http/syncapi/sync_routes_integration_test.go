62c383a7fa4b162f4a0e646d49412c27c1ac6c1a
Jed Walton <jedwaltondev@gmail.com>
2023-11-02 03:45:06 +0000

    prompts integration tests added also

--- backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
+++ backend/lucidify-api/http/syncapi/sync_routes_integration_test.go
@@ -531,3 +531,223 @@ func TestFoldersIntegration(t *testing.T) {
 		// postgresqlDB.DeleteDocument(UserID, "Test Document")
 	})
 }
+
+func TestPromptsIntegration(t *testing.T) {
+	setup := SetupTestEnvironment(t)
+	cfg := setup.Config
+	postgresqlDB := setup.PostgresqlDB
+	clerkInstance := setup.ClerkInstance
+
+	syncService, err := syncservice.NewSyncService()
+	if err != nil {
+		t.Fatalf("Failed to create SyncService: %v", err)
+	}
+
+	// Create a test server
+	mux := http.NewServeMux()
+	SetupRoutes(cfg, mux, clerkInstance, syncService)
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	// Obtain a JWT token from Clerk
+	jwtToken := cfg.TestJWTSessionToken
+
+	// Send a POST request to the server with the JWT token
+	body, _ := json.Marshal("somePromptsData")
+
+	// Authenticated request
+	req, _ := http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"message":"Data set successfully for key: prompts"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"message":"Data set successfully for key: prompts"}`,
+			string(respBody))
+	}
+
+	// Autheticated Request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"data":"\"somePromptsData\"","message":"Data fetched successfully"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"data":"\"somePromptsData\"","message":"Data fetched successfully"}`,
+			string(respBody))
+	}
+
+	// Testing the on conflict (postgres) functionality to update chat
+	// Send a POST request to the server with the JWT token
+	body, _ = json.Marshal("somePromptsData but updated!")
+
+	// Authenticated request
+	req, _ = http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"message":"Data set successfully for key: prompts"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"message":"Data set successfully for key: prompts"}`,
+			string(respBody))
+	}
+
+	// Autheticated Request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	if string(respBody) !=
+		`{"success":true,"data":"\"somePromptsData but updated!\"","message":"Data fetched successfully"}` {
+		t.Errorf("Expected response body %s, got %s",
+			`{"success":true,"data":"\"somePromptsData but updated!\"","message":"Data fetched successfully"}`,
+			string(respBody))
+	}
+
+	// Unauthenticated POST request
+	req, _ = http.NewRequest(
+		http.MethodPost,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken+"invalid")
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	// This is counter intuitive. Not sure why clerk doesn't return a 401.
+	if string(respBody) != `couldn't find cookie __session` {
+		t.Errorf("Expected response body %s, got %s", `couldn't find cookie __session`, string(respBody))
+	}
+	// Unauthenticated GET request
+	req, _ = http.NewRequest(
+		http.MethodGet,
+		server.URL+"/api/sync/localstorage/?key=prompts",
+		bytes.NewBuffer(body))
+
+	req.Header.Set("Authorization", "Bearer "+jwtToken+"invalid")
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Read the response body
+	respBody, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body: %v", err)
+	}
+
+	// This is counter intuitive. Not sure why clerk doesn't return a 401.
+	if string(respBody) != `couldn't find cookie __session` {
+		t.Errorf("Expected response body %s, got %s",
+			`couldn't find cookie __session`, string(respBody))
+	}
+
+	// Cleanup the database
+	t.Cleanup(func() {
+		testconfig := config.NewServerConfig()
+		UserID := testconfig.TestUserID
+		postgresqlDB.DeleteUserInUsersTable(UserID)
+		// postgresqlDB.DeleteDocument(UserID, "Test Document")
+	})
+}
